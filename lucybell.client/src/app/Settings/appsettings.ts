8813bab6f17d770d9c3e347720310cbc30489263
Thomas Dergazarian <Dergazarian21@hotmail.com>
2024-09-02 08:46:50 -0300

    Correccion estilo tablas categorias

--- lucybell.client/src/app/Settings/appsettings.ts
+++ lucybell.client/src/app/Settings/appsettings.ts
@@ -1,4 +1,4 @@
 export const appsettings ={
-  apiUrl: 'https://www.lucybelljoyas.somee.com/api/'
-  /*apiUrl: 'https://localhost:7123/api/'*/
+  /*apiUrl: 'https://www.lucybelljoyas.somee.com/api/'*/
+  apiUrl: 'https://localhost:7123/api/'
 }
