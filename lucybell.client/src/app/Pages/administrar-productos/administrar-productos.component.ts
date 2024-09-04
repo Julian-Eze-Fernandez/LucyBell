174d4c533eda39ceeab803cdcf14ba0715eae311
Thomas Dergazarian <Dergazarian21@hotmail.com>
2024-09-04 14:48:41 -0300

    Categoria de AdministrarProductos arreglada. Agregado botones sin logica.

--- lucybell.client/src/app/Pages/administrar-productos/administrar-productos.component.ts
+++ lucybell.client/src/app/Pages/administrar-productos/administrar-productos.component.ts
@@ -4,6 +4,7 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ProductoService } from '../../Services/producto.service'
 import { CategoriaService } from '../../Services/categoria.service'
 import {Producto} from '../../Models/Producto'
+import {Categoria} from '../../Models/Categoria'
 
 @Component({
   selector: 'app-administrar-productos',
@@ -15,19 +16,39 @@ import {Producto} from '../../Models/Producto'
 export class AdministrarProductosComponent implements OnInit {
 
   productos: Producto[] = [];
+  categorias: Categoria[] = []
+  categoriasMap: { [id: number]: string } = {};
 
-  constructor(private productoService: ProductoService) { }
+  constructor(private productoService: ProductoService, private categoriaService: CategoriaService) { }
 
   ngOnInit(): void {
+    this.cargarProductos()
+  }
+
+  cargarProductos(): void {
     this.productoService.GetProductoCompleto().subscribe((data: Producto[]) => {
       this.productos = data;
+
+      const categoriaIds = Array.from(this.productos.map(p => p.categoriaId));
+
       console.log(this.productos);
       console.log(this.productos[0].imagenesProductos[0].urlImagen);
-      
+
+      categoriaIds.forEach(id => {
+        this.categoriaService.obtener(id).subscribe(categoria => {
+          this.categoriasMap[id] = categoria.nombre;
+        });
+      });
     });
+
   }
 
   getTotalStock(producto: Producto): number {
     return producto.variantesProducto.reduce((acc, variante) => acc + variante.cantidad, 0);
   }
+
+  getCategoryName(id: number): string {
+    return this.categoriasMap[id] || 'Desconocido';
+  }
+
 }
