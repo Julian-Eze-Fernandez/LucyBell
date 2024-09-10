bef4222b4cc07af0ad320c773b0993e8bf993eb1
Thomas Dergazarian <Dergazarian21@hotmail.com>
2024-09-10 12:03:07 -0300

    Avanze de delete grid stock

--- lucybell.client/src/app/Pages/stock/stock.component.ts
+++ lucybell.client/src/app/Pages/stock/stock.component.ts
@@ -1,15 +1,16 @@
-import { Component, model, OnInit  } from '@angular/core';
+import { Component, model, OnInit, ViewChild  } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { CommonModule, } from '@angular/common';
 import {VariantesProductoService} from '../../Services/variantes-producto.service';
 import {VarianteProductoDTO} from '../../Models/VariantesProducto';
 import { ProductoService } from '../../Services/producto.service';
 import { Producto } from '../../Models/Producto';
+import { TwoButtonModalComponent } from '../../Pages/two-button-modal/two-button-modal.component'
 
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports:[CommonModule, SidebarComponent] ,
+  imports: [CommonModule, SidebarComponent, TwoButtonModalComponent],
   templateUrl: './stock.component.html',
   styleUrl: './stock.component.css'
 })
@@ -17,9 +18,13 @@ export class StockComponent implements OnInit {
 
   constructor(private VariantesProductoService: VariantesProductoService, private ProductoService: ProductoService) {}
 
- 
+ @ViewChild('deleteModalStock') deleteModalStock!: TwoButtonModalComponent;
+  
  variantesConProducto: { productoNombre: string, variante: VarianteProductoDTO }[] = [];
- productos: Producto[] = [];
+  productos: Producto[] = [];
+  selectedVariante: VarianteProductoDTO | null = null;
+  customIcon = '';
+
 
 
  ngOnInit(): void {
@@ -36,6 +41,40 @@ export class StockComponent implements OnInit {
       );
     });
   }
+
+  openDeleteStockModal(variante: any) {
+
+    this.selectedVariante = variante;
+    this.deleteModalStock.openModal();
+
+  }
+  closeDeleteStockModal() {
+
+    this.selectedVariante = null;
+
+    this.deleteModalStock.closeModal();
+
+  }
+
+  onDelete(): void {
+    if (this.selectedVariante) {
+
+      this.VariantesProductoService.deleteVariantesProducto(this.selectedVariante.id).subscribe({
+        next: (response) => {
+          if (response) {
+            // Only close the modal and reload products if the update was successful
+            this.closeDeleteStockModal();
+            this.cargarStock();
+          }
+        },
+        error: (err) => {
+          console.error('Error deleting product:', err);
+        }
+      })
+    }
+
+  }
+
 }
     
 
