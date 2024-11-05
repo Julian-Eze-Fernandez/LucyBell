ca205c7b260e0dad6dc8e3bda7a7d3d2b0365ca9
Julian Fernandez <fernandezjulian00@hotmail.com>
2024-11-05 16:24:04 -0300

    Se añadio una scrollbar al carrito y cuando se cierra sesion se limpia el localstorage

--- lucybell.client/src/app/Services/seguridad.service.ts
+++ lucybell.client/src/app/Services/seguridad.service.ts
@@ -79,6 +79,7 @@ export class SeguridadService {
   logout(){
     localStorage.removeItem(this.llaveToken);
     localStorage.removeItem(this.llaveExpiracion);
+    localStorage.removeItem('carrito'); 
   }
 
   obtenerRol(): string {
